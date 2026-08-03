94a40f3ed1db76dc84a2f5a6187ae7c559cfd52d
Kamesh-024 <david7danny@gmail.com>
2026-08-03 23:31:09 +0530

    solved find all anagrams in string using sliding window + frequency array

---
+++ Arrays/438.find_all_anagrams_in_string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] pcount=new int[26];
+        int[] scount=new int[26];
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<p.length();i++){
+            pcount[p.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            scount[s.charAt(i)-'a']++;
+
+            if(i>=p.length()){
+                scount[s.charAt( i-p.length() ) - 'a']--;
+            }
+
+            if(Arrays.equals(scount,pcount)){
+                list.add(i-p.length() + 1);
+            }
+        }
+        return list;
+    }
+}
